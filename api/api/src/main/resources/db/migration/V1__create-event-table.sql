8fafec0b874f81bb88d36562b84cf060faa6ccc3
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2025-03-23 15:38:27 -0400

    refactor: caps lock e varchar(250)

--- api/api/src/main/resources/db/migration/V1__create-event-table.sql
+++ api/api/src/main/resources/db/migration/V1__create-event-table.sql
@@ -1,9 +1,9 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE event (
-    id UUID default gen_random_uuid() PRIMARY KEY,
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    description VARCHAR(250) NOT NULL,
     img_url VARCHAR(100) NOT NULL,
     event_url VARCHAR(100) NOT NULL,
     date TIMESTAMP NOT NULL,
